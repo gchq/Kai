7e1f96a4a0082e00eecd715e3c74e3383a574e95
t11947 <t11947@github.noreply.example.com>
2019-10-28 14:47:41 +0000

    gh-3 Added junit and test for travis

---
+++ src/test/java/uk/gov/gchq/kai/KaiStackTest.java
@@ -0,0 +1,12 @@
+package uk.gov.gchq.kai;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class KaiStackTest {
+
+    @Test
+    public void shouldBeTrueForTravis() {
+        Assert.assertTrue(true);
+    }
+}
