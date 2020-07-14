780071f0e357b8bdfd802e5c85f5868dacc610c5
K-itKat20 <LaurelKatie@tutanota.com>
2020-07-14 11:25:02 +0100

    Created a table in the UI that displays the response from the API. Also added in basic nnavigation

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import {BrowserRouter, Route} from "react-router-dom";
 import { RestClient } from '../rest/rest-client';
+import ExampleTable from '../components/Tables/ExampleTable';
+import {Container, Grid} from '@material-ui/core'
+import Navigation from '../components/Navigation/Navigation'
 
 interface IState {
   graphs: Array<Object>;
@@ -16,13 +19,28 @@ class App extends React.Component<{},IState> {
   public async componentDidMount() {
     const graphs = await RestClient.getAllGraphs();
     this.setState({ graphs: graphs })
-  }
+  };
 
   public render() {
     return (
       <div className="App">
-        <span>Hello Word</span>
-        <p>{JSON.stringify(this.state.graphs)}</p>
+         <Grid style={{marginTop: 60}}
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center">
+                <Grid item>
+                    <Navigation/>
+                </Grid>
+                <Grid item style={{marginLeft: 100}}>
+                    <BrowserRouter basename="/">
+                        <Route exact path={"/"}>
+                        </Route>
+                    </BrowserRouter>
+                </Grid>
+            </Grid>
+
+        
       </div>
     );
   }
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 import './global.scss';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+</React.StrictMode>,
   document.getElementById('root')
 );
 
