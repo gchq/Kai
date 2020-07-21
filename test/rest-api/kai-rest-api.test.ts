2beafe45c4f186d795d75f11f7bbe58d09a5a89f
p3430233 <p3430233@outlook.com>
2020-07-21 09:45:37 +0100

    DELETE HttpMethod ResourcId Ref mismatch fixed

--- test/rest-api/kai-rest-api.test.ts
+++ test/rest-api/kai-rest-api.test.ts
@@ -114,7 +114,7 @@ test("The REST API should have a resource which can GET specific graphs", () => 
     expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Method", {
         HttpMethod: "GET",
         ResourceId: {
-            Ref: "TestTestRestApigraphsgraphName0A18A4C6"
+            Ref: "TestTestRestApigraphs6F3DCBD4"
         },
         RestApiId: {
             Ref: "TestTestRestApiF3AB3CBC"
@@ -191,7 +191,7 @@ test("The specific Graph resource should handle DELETE requests", () => {
     expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Method", {
         HttpMethod: "DELETE",
         ResourceId: {
-            Ref: "TestTestRestApigraphsgraphName0A18A4C6"
+            Ref: "TestTestRestApigraphsgraphNameB9AC8DA7"
         },
         RestApiId: {
             Ref: "TestTestRestApiF3AB3CBC"
