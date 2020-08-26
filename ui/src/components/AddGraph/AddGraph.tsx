ca8843ec8a263eb6dd42eecec1281ce158810066
jpelbertrios <elbertrios@protonmail.com>
2020-08-26 16:51:45 +0100

    UI changes from new UX

--- ui/src/components/AddGraph/AddGraph.tsx
+++ ui/src/components/AddGraph/AddGraph.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Container, CssBaseline, Grid, IconButton, makeStyles, TextField, Typography} from "@material-ui/core";
+import {Button, Container, CssBaseline, Grid, IconButton, makeStyles, TextField, Typography, Toolbar} from "@material-ui/core";
 import {Schema} from '../../domain/schema';
 import {Notifications} from '../../domain/notifications';
 import {CreateGraphRepo} from '../../rest/repositories/create-graph-repo';
@@ -72,99 +72,102 @@ export default class AddGraph extends React.Component<{}, IState> {
 
     public render() {
         return (
-            <Grid style={{marginTop:30}}
-                  container
-                  justify="center">
-
-
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline/>
-                    {!this.state.notifications.isEmpty() &&
-                    <Alert variant="outlined"
-                           severity="error">Error(s): {this.state.notifications.errorMessage()}</Alert>}
-                    <div className={this.classes.paper}>
-
-                        <form className={this.classes.form} noValidate>
-
-
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-
-                                    <TextField
-                                        variant="outlined"
-                                        required
-                                        fullWidth
-                                        id="graphId"
-                                        label="Graph ID"
-                                        name="graphId"
-                                        autoComplete="graph-id"
-                                        onChange={(event) => {
-                                            this.setState({
-                                                newGraph: {
-                                                    ...this.state.newGraph,
-                                                    graphId: event.target.value
-                                                }
-                                            });
-                                        }}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}
-                                      container
-                                      direction="row"
-                                      justify="flex-end"
-                                      alignItems="center"
-                                >
-                                    <IconButton>
-                                        <InsertDriveFileOutlinedIcon/>
-                                    </IconButton>
-                                    <IconButton>
-                                        <DeleteOutlineOutlinedIcon/>
-                                    </IconButton>
-
-                                </Grid>
-
-                                <Grid item xs={12}>
-
-                                    <TextField
-                                        style={{width: 400}}
-                                        id="schema"
-                                        label="Schema"
-                                        required
-                                        multiline
-                                        rows={15}
-                                        variant="outlined"
-                                        onChange={(event) => {
-                                            this.setState({
-                                                newGraph: {
-                                                    ...this.state.newGraph,
-                                                    schemaJson: event.target.value
-                                                }
-                                            });
-                                        }}
-                                    />
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </div>
-                </Container>
-                <Grid
+            <main>
+                <Toolbar />
+                <Grid style={{marginTop:30}}
                     container
-                    style={{margin:10}}
-                    direction="row"
-                    justify="center"
-                    alignItems="center">
-                    <Button onClick={() => {
-                        this.submitNewGraph()
-                    }}
-                            type="submit"
-                            variant="outlined"
-                            color="primary"
-                            className={this.classes.submit}
-                    >
-                        Add Graph
-                    </Button>
+                    justify="center">
+
+
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline/>
+                        {!this.state.notifications.isEmpty() &&
+                        <Alert variant="outlined"
+                            severity="error">Error(s): {this.state.notifications.errorMessage()}</Alert>}
+                        <div className={this.classes.paper}>
+
+                            <form className={this.classes.form} noValidate>
+
+
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+
+                                        <TextField
+                                            variant="outlined"
+                                            required
+                                            fullWidth
+                                            id="graphId"
+                                            label="Graph ID"
+                                            name="graphId"
+                                            autoComplete="graph-id"
+                                            onChange={(event) => {
+                                                this.setState({
+                                                    newGraph: {
+                                                        ...this.state.newGraph,
+                                                        graphId: event.target.value
+                                                    }
+                                                });
+                                            }}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}
+                                        container
+                                        direction="row"
+                                        justify="flex-end"
+                                        alignItems="center"
+                                    >
+                                        <IconButton>
+                                            <InsertDriveFileOutlinedIcon/>
+                                        </IconButton>
+                                        <IconButton>
+                                            <DeleteOutlineOutlinedIcon/>
+                                        </IconButton>
+
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+
+                                        <TextField
+                                            style={{width: 400}}
+                                            id="schema"
+                                            label="Schema"
+                                            required
+                                            multiline
+                                            rows={15}
+                                            variant="outlined"
+                                            onChange={(event) => {
+                                                this.setState({
+                                                    newGraph: {
+                                                        ...this.state.newGraph,
+                                                        schemaJson: event.target.value
+                                                    }
+                                                });
+                                            }}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </div>
+                    </Container>
+                    <Grid
+                        container
+                        style={{margin:10}}
+                        direction="row"
+                        justify="center"
+                        alignItems="center">
+                        <Button onClick={() => {
+                            this.submitNewGraph()
+                        }}
+                                type="submit"
+                                variant="outlined"
+                                color="primary"
+                                className={this.classes.submit}
+                        >
+                            Add Graph
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </main>
         )
     }
 }
