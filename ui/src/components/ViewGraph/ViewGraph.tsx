8b2e57fb00d9cfbe99ae32643951fbb44ee12c25
macenturalxl1 <macenturalxl1@tutanota.com>
2020-09-11 14:35:35 +0100

    Reformatted ViewGraphs

--- ui/src/components/ViewGraph/ViewGraph.tsx
+++ ui/src/components/ViewGraph/ViewGraph.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import { Grid, Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Toolbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Toolbar } from '@material-ui/core'
 import { Graph } from '../../domain/graph';
-import { DeleteGraphRepo } from '../../rest/repositories/delete-graph-repo';
 import { GetAllGraphsRepo } from '../../rest/repositories/get-all-graphs-repo';
-import CreateIcon from '@material-ui/icons/Create';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-import PageNotFound from '../Errors/PageNotFound';
 
 interface IState {
     graphs: Graph[],
@@ -25,11 +22,11 @@ export default class ViewGraph extends React.Component<{}, IState> {
     public async componentDidMount() {
         try {
             this.setState({ graphs: await new GetAllGraphsRepo().getAll() });
-        } catch (error) { 
+        } catch (error) {
             console.log("Graph not found");
         }
     }
-    
+
     private classes: any = makeStyles({
         root: {
             width: '100%',
@@ -40,46 +37,45 @@ export default class ViewGraph extends React.Component<{}, IState> {
         },
     });
 
-    private getStripedStyle = (index:any) => {
+    private getStripedStyle = (index: any) => {
         return { background: index % 2 ? '#fafafa' : '#fafafa' };
     }
 
     public render() {
-        
+
         return (
-           <main style={{marginTop:30}}>
-               <Toolbar />
+            <main style={{ marginTop: 30 }}>
+                <Toolbar />
                 <Box width="50%" m="auto">
                     <TableContainer>
                         <Table className={this.classes.table} aria-label="simple table" >
-                    
+
                             <TableHead>
-                                <TableRow style = {{background: '#F4F2F2'}}>
+                                <TableRow style={{ background: '#F4F2F2' }}>
                                     <TableCell>Graph Name</TableCell>
                                     <TableCell align="right">Current State</TableCell>
                                     <TableCell align="right">Delete</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                     {this.state.graphs.map((graph: Graph, index) => (
-                                        <TableRow key={graph.getId()} hover role="checkbox"
-                                                onClick={() => this.setState({selectedRow: graph.getId()})} style = {{ ...this.getStripedStyle(index)}}>
-    
-                                            <TableCell component="th" scope="row">{graph.getId()}</TableCell>
-                                            <TableCell align="right">{graph.getStatus()}</TableCell>
-                                            <TableCell align="right">
-                                                <IconButton>
-                                                    <DeleteOutlineOutlinedIcon/>
-                                                </IconButton>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
+                                {this.state.graphs.map((graph: Graph, index) => (
+                                    <TableRow key={graph.getId()} hover role="checkbox"
+                                        onClick={() => this.setState({ selectedRow: graph.getId() })} style={{ ...this.getStripedStyle(index) }}>
+
+                                        <TableCell component="th" scope="row">{graph.getId()}</TableCell>
+                                        <TableCell align="right">{graph.getStatus()}</TableCell>
+                                        <TableCell align="right">
+                                            <IconButton>
+                                                <DeleteOutlineOutlinedIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
                             </TableBody>
                         </Table>
                     </TableContainer>
                 </Box>
-           </main>      
-
-        ); 
+            </main>
+        );
     }
 }
