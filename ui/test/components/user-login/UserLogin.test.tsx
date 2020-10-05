13ad1b3a4cef0a30cf836106716e14e333715f26
jpelbertrios <elbertrios@protonmail.com>
2020-10-05 10:40:15 +0100

    Update unit test for user login

--- ui/test/components/user-login/UserLogin.test.tsx
+++ ui/test/components/user-login/UserLogin.test.tsx
@@ -7,19 +7,41 @@ beforeEach(() => (wrapper = mount(<UserLogin />)));
 afterEach(() => wrapper.unmount());
 
 describe('On Render', () => {
-    it('should have a username input field', () => {
+    it('should have a new username input field', () => {
         const inputfield = wrapper.find('input');
         expect(inputfield.at(0).props().name).toBe('username');
     });
+    it('should have a temporary password input field', () => {
+        const textfield = wrapper.find('input');
+        expect(textfield.at(1).props().name).toBe('temppassword');
+    });
+    it('should have a new password input field', () => {
+        const textfield = wrapper.find('input');
+        expect(textfield.at(2).props().name).toBe('newpassword');
+    });
+    it('should have a username input field', () => {
+        const textfield = wrapper.find('input');
+        expect(textfield.at(3).props().name).toBe('username');
+    });
     it('should have a password input field', () => {
         const textfield = wrapper.find('input');
-        expect(textfield.at(1).props().name).toBe('password');
+        expect(textfield.at(4).props().name).toBe('password');
+    });
+
+    it('should have a Update Password button', () => {
+        const submitButton = wrapper.find('button').at(0).text();
+        expect(submitButton).toBe('Update Password');
     });
     it('should have a sign in button', () => {
-        const submitButton = wrapper.find('button').at(0).text();
+        const submitButton = wrapper.find('button').at(1).text();
         expect(submitButton).toBe('Sign In');
     });
 });
+
+describe('Update Password Button', () => {
+
+});
+
 describe('Sign in Button', () => {
     it('should be disabled when Username and Password fields are empty', () => {
         expect(wrapper.find('button#sign-in-button').props().disabled).toBe(true);
